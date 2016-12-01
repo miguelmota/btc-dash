361a8b752c615b1a7cd7d140daa90a623ec714f4
Miguel Mota <miguelmota2@gmail.com>
2016-11-30 16:31:13 -0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# BTC/ETC dash
+
+> A dashboard for terminal that displays historical price indexes for bitcoin and ether.
+
+<img src="./screenshot.png" width="600">
+
+# Run
+
+```bash
+npm start
+```
+
+# License
+
+MIT
+
+
---
+++ index.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const moment = require(`moment`);
+const _ = require(`lodash`);
+
+const Api = require(`./lib/Api`);
+const Gui = require(`./lib/Gui`);
+
+function init() {
+  const gui = new Gui();
+  const timeOptions = [1, 7, 30, 90, 365];
+  const timeOption = 4;
+
+  const start =  moment().subtract(timeOptions[timeOption], `day`).unix();
+  const end = moment().unix();
+
+  const renderFetch = () => {
+    Api.getBTCPrices({
+      start,
+      end
+    })
+    .then(({x, y}) => {
+      return Api.getBTCCurrentPrice()
+      .then(price => {
+        gui.renderChart({data: {x, y}, position: [0, 0, 5, 10], label: `BTC`, title: `Current ${price}`});
+      });
+    });
+
+    Api.getETCPrices({
+      start,
+      end
+    })
+    .then(({x, y}) => {
+      const price = _.last(y);
+      gui.renderChart({data: {x, y}, position: [5, 0, 5, 10], label: `ETC`, title: `Current ${price}`});
+    });
+  };
+
+  renderFetch();
+
+  setInterval(() => {
+    renderFetch();
+  }, 10);
+}
+
+init();
